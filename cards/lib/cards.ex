89176d664e59133e7b987e8d27b7ab74c6b81a1d
Balint Orosz <orosz.balint.97@gmail.com>
2023-10-26 22:12:41 +0200

    add types

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,4 +1,7 @@
 defmodule Cards do
+  @type deck() :: list(String)
+
+  @spec create_deck() :: deck()
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Cluds", "Hearts", "Diamonds"]
@@ -8,11 +11,18 @@ defmodule Cards do
     end
   end
 
-  def shuffle(deck) do
+  @spec shuffle(deck :: deck()) :: list(String)
+  def shuffle(deck) when is_list(deck) do
     Enum.shuffle(deck)
   end
 
+  @spec contains?(deck :: deck(), card :: String) :: boolean
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  @spec deal(deck :: deck(), hand_size :: integer()) :: {list(String), list(String)}
+  def deal(deck, hand_size) when is_list(deck) and is_integer(hand_size) do
+    Enum.split(deck, hand_size)
   end
 end
