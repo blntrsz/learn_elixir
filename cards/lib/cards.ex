bf1f52cb1883e134211c6aa619c5e39442637ac9
Balint Orosz <orosz.balint.97@gmail.com>
2023-10-28 17:25:50 +0200

    add docs and tests

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,6 +1,12 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
   @type deck() :: list(String)
 
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   @spec create_deck() :: deck()
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
@@ -16,11 +22,31 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whather a deck contains a given card
+
+  ## Examples
+    
+    iex> deck = Cards.create_deck
+    iex> Cards.contains?(deck, "Ace of Spades")
+    true
+  """
   @spec contains?(deck :: deck(), card :: String) :: boolean
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicated how many cards should be in hand.
+
+  ## Examples
+    
+    iex> deck = Cards.create_deck
+    iex> {hand, _deck} = Cards.deal(deck, 1)
+    iex> hand
+    ["Ace of Spades"]
+  """
   @spec deal(deck :: deck(), hand_size :: integer()) :: {list(String), list(String)}
   def deal(deck, hand_size) when is_list(deck) and is_integer(hand_size) do
     Enum.split(deck, hand_size)
